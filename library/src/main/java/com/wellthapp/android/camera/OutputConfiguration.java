7345457bd802cd8ba944772c624b939daa41bd46
Kyle Weisel <kyleweisel@gmail.com>
2017-11-14 09:44:50 -0500

    Added fromString method to directory enum

--- library/src/main/java/com/wellthapp/android/camera/OutputConfiguration.java
+++ library/src/main/java/com/wellthapp/android/camera/OutputConfiguration.java
@@ -57,9 +57,43 @@ public final class OutputConfiguration {
     }
 
     public enum Directory {
-        Cache,
-        Private,
-        Public
+
+        CACHE("cache"),
+        PRIVATE("private"),
+        PUBLIC("public");
+
+        public static final String TAG = "Directory";
+        private final String text;
+
+        public static Directory fromString(final String text) {
+            if (text != null) {
+                for (final Directory directory : Directory.values()) {
+                    if (directory.toString().equalsIgnoreCase(text)) {
+                        return directory;
+                    }
+                }
+            }
+            // Return cache as the default
+            return Directory.CACHE;
+        }
+
+        /**
+         * Constructor from String
+         * @param text is a String that represents this (soon to be) instance
+         */
+        Directory(final String text) {
+            this.text = text;
+        }
+
+        /**
+         * Converts this model into a String
+         * @return a String representing this instance
+         */
+        @Override
+        public final String toString() {
+            return this.text;
+        }
+
     }
 
     public enum Rotation {
